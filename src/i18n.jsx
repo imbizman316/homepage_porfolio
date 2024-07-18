4e40abb3c6d2d02610edc4e96ef7ea7d707705bc
Mikey <pride81_@hotmail.com>
2024-07-18 23:37:08 +0900

    Edit localization

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -15,6 +15,12 @@ i18n.use(initReactI18next).init({
         about_descr:
           "I was born in Seoul, South Korea but moved to Canada when I was 15 years old. I returned to Korea in 2007, and got my foot in the game industry as as assistant manager in the overseas business team. After being in the industry more than 10 years, I finally deicded to change my position to coding, which I'd wanted to do for a long time.",
         portfolio_title: "Here are the projects I've built.",
+        portfolio_project_1: "K-pop Group Page Mock-up",
+        portfolio_project_1_desc:
+          "It's a mock-up website of a k-pop girl group called TripleS.  It's almost identical to it except that I added a newletter subscription component where users can submit their emails.  Sent emails get saved in MongoDB database.",
+        portfolio_project_2: "Blog Page",
+        portfolio_project_2_desc:
+          "It's a website I made for my friend who is a writer.  He can login using his google account, and write a blog.  Saved blogs are stored in MongoDB, and loads from it.",
       },
     },
     kr: {
@@ -25,6 +31,12 @@ i18n.use(initReactI18next).init({
         about_descr:
           "게임업계 사업부에서 마케팅/커뮤니티메니저/해외사업 등을 담당했지만 개발의 꿈에 도전해 지금은 Front-end 개발자로서의 길을 걸어가고 있습니다.",
         portfolio_title: "지금까지 작업한 프로젝트들입니다.",
+        portfolio_project_1: "K-pop 걸그룹 (트리플S) 사이트",
+        portfolio_project_1_desc:
+          "트리플S 사이트를 본따서 만든 사이트입니다. 해당 사이트와 거의 동일하지만, 이메일을 입력해서 뉴스레터 구독을 할 수 있게 추가하였습니다.  받은 이메일은 MongoDB에 저장되게 하였습니다.",
+        portfolio_project_2: "블로그 페이지",
+        portfolio_project_2_desc:
+          "작가인 지인을 위해 만든 사이트입니다.  구글 계정으로 로그인하면 글을 올릴 수 있게 했으며, 글들은 MongoDB에 저장된 후, 사이트 방문자들이 볼 수 있게 하였습니다.",
       },
     },
   },
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -3,55 +3,6 @@ import ProjectItem from "./components/ProjectItem";
 import { useTranslation } from "react-i18next";
 import { useAppContext } from "../../components/context";
 
-const portfolio = [
-  {
-    id: 0,
-    title: "K-pop Group Page Mock-up",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam eligendi non culpa enim soluta rerum debitis nulla, praesentium laborum consequatur aliquam voluptate provident. Quo eius velit laboriosam facilis atque.",
-    image: "https://i.imgur.com/ECYvhiZ.png",
-    languages: ["NextJS", "Tailwind", "MongoDB"],
-    github: "https://github.com/imbizman316/gorgeous_nextjs",
-    demo: "https://gorgeous-nextjs.vercel.app/",
-  },
-  // {
-  //   id: 1,
-  //   title: "FlixPatrol Clone",
-  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam eligendi non culpa enim soluta rerum debitis nulla, praesentium laborum consequatur aliquam voluptate provident. Quo eius velit laboriosam facilis atque.",
-  //   image:
-  //     "https://miro.medium.com/v2/resize:fit:1400/1*fGz9RM5dBbdBYQCBMDPr2Q.jpeg",
-  //   languages: ["React", "Tailwind"],
-  //   github: "https://github.com/imbizman316/flixpatrol_clone",
-  //   demo: "https://flixpatrol-clone.vercel.app/",
-  // },
-  // {
-  //   id: 2,
-  //   title: "Ticketing App",
-  //   desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae beatae tempore maxime a, reprehenderit, repellendus illo laborum eum cumque tenetur nostrum. Cumque debitis quidem est? Illo sit provident perferendis.",
-  //   image: "https://i.imgur.com/7ZMmJd5.png",
-  //   languages: ["Next.js", "Tailwind"],
-  //   github: "https://github.com/imbizman316/ticket_app_nextjs",
-  //   demo: "https://ticket-app-nextjs-seven.vercel.app/",
-  // },
-  {
-    id: 3,
-    title: "Blog App",
-    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae beatae tempore maxime a, reprehenderit, repellendus illo laborum eum cumque tenetur nostrum. Cumque debitis quidem est? Illo sit provident perferendis.",
-    image: "https://i.imgur.com/urhEZjW.png",
-    languages: ["Next.js", "Tailwind", "MongoDB"],
-    github: "https://github.com/imbizman316/jhkim",
-    demo: "https://jhkim-pink.vercel.app/",
-  },
-  // {
-  //   id: 2,
-  //   title: "SEX GYM",
-  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat numquam distinctio libero nemo ipsa exercitationem perspiciatis architecto rem, facere maiores deleniti repudiandae dicta odit? Blanditiis possimus natus eius expedita aspernatur?",
-  //   image: "https://themewagon.com/wp-content/uploads/2020/10/gym-1.jpg",
-  //   languages: ["React", "Tailwind"],
-  //   github: "https://github.com/imbizman316/homepage",
-  //   demo: "https://0402-shopping-cart.vercel.app/cart",
-  // },
-];
-
 function Projects() {
   const { t, i18n } = useTranslation();
   const { language } = useAppContext();
@@ -59,6 +10,55 @@ function Projects() {
   useEffect(() => {
     i18n.changeLanguage(language);
   }, [i18n, language]);
+
+  const portfolio = [
+    {
+      id: 0,
+      title: t("portfolio_project_1"),
+      desc: t("portfolio_project_1_desc"),
+      image: "https://i.imgur.com/ECYvhiZ.png",
+      languages: ["NextJS", "Tailwind", "MongoDB"],
+      github: "https://github.com/imbizman316/gorgeous_nextjs",
+      demo: "https://gorgeous-nextjs.vercel.app/",
+    },
+    // {
+    //   id: 1,
+    //   title: "FlixPatrol Clone",
+    //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam eligendi non culpa enim soluta rerum debitis nulla, praesentium laborum consequatur aliquam voluptate provident. Quo eius velit laboriosam facilis atque.",
+    //   image:
+    //     "https://miro.medium.com/v2/resize:fit:1400/1*fGz9RM5dBbdBYQCBMDPr2Q.jpeg",
+    //   languages: ["React", "Tailwind"],
+    //   github: "https://github.com/imbizman316/flixpatrol_clone",
+    //   demo: "https://flixpatrol-clone.vercel.app/",
+    // },
+    // {
+    //   id: 2,
+    //   title: "Ticketing App",
+    //   desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil repudiandae beatae tempore maxime a, reprehenderit, repellendus illo laborum eum cumque tenetur nostrum. Cumque debitis quidem est? Illo sit provident perferendis.",
+    //   image: "https://i.imgur.com/7ZMmJd5.png",
+    //   languages: ["Next.js", "Tailwind"],
+    //   github: "https://github.com/imbizman316/ticket_app_nextjs",
+    //   demo: "https://ticket-app-nextjs-seven.vercel.app/",
+    // },
+    {
+      id: 3,
+      title: t("portfolio_project_2"),
+      desc: t("portfolio_project_2_desc"),
+      image: "https://i.imgur.com/urhEZjW.png",
+      languages: ["Next.js", "Tailwind", "MongoDB"],
+      github: "https://github.com/imbizman316/jhkim",
+      demo: "https://jhkim-pink.vercel.app/",
+    },
+    // {
+    //   id: 2,
+    //   title: "SEX GYM",
+    //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat numquam distinctio libero nemo ipsa exercitationem perspiciatis architecto rem, facere maiores deleniti repudiandae dicta odit? Blanditiis possimus natus eius expedita aspernatur?",
+    //   image: "https://themewagon.com/wp-content/uploads/2020/10/gym-1.jpg",
+    //   languages: ["React", "Tailwind"],
+    //   github: "https://github.com/imbizman316/homepage",
+    //   demo: "https://0402-shopping-cart.vercel.app/cart",
+    // },
+  ];
 
   return (
     <div className="flex flex-col items-center w-full">
